5c0ded1075da24de349fe19dfbb60264895a4767
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-09-02 12:29:33 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa que soma dois números");
+
+  System.out.println(" o primeiro número é");
+  int numero1= 4 ;
+
+  System.out.println(numero1);
+    
+    System.out.println("o segundo número é");
+    int numero2= 6;
+
+  System.out.println(numero2);
+System.out.println("A soma dos dois números é");
+    int soma = numero1 + numero2;
+
+    System.out.println(soma);
+
+
+   
+    
+    }
+}
